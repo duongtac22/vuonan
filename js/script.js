a5dac7408b094e48a3e5bfea2c2227e77d91be90
duongta <duongtruonganh@vccorp.vn>
2021-04-27 00:54:33 +0700

    asdasdasda

--- js/script.js
+++ js/script.js
@@ -1,23 +1,81 @@
 // $('.lazyload').lazy();
+function demnguoc(endTime, holder) {
+    var days = Math.floor(endTime / (60 * 60 * 24));
+    var hours = Math.floor((endTime % (60 * 60 * 24)) / (60 * 60));
+    var minutes = Math.floor((endTime % (60 * 60)) / 60);
+    var seconds = Math.floor(endTime % 60);
+
+    $(holder).find('.js-day').html(days);
+    $(holder).find('.js-hour').html(hours);
+    $(holder).find('.js-minutes').html(minutes);
+    $(holder).find('.js-secs').html(seconds);
+
+    setTimeout(function () {
+        demnguoc(endTime - 1, holder);
+    }, 1000);
+    }
+
 $(document).ready(function() {
     $(".lazy").slick({
         slidesToShow: 1,
         slidesToScroll:1,
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
     });
     $(".regular").slick({
         dots: true,
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll:1
+        slidesToScroll:1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+          ]
     });
-
+    $(".review-home-slider").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll:1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+          ]
+    });
     $(".regular-2").slick({
         dots: true,
         infinite: true,
         slidesToShow: 4,
         slidesToScroll:1
     });
+    if($('.js-count').length > 0 ) {
+        $(".js-count").each(function () {
+            let endTime = $(this).attr("data-end");
+            demnguoc(endTime, this);
+        });
+    }
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
             $('#back-to-top').fadeIn();
